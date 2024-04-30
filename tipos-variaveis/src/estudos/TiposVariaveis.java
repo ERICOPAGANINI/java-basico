422dcfff14412a0165f0aa9ba761100cdfd20613
ERICOPAGANINI <erico.paganini@gmail.com>
2024-04-30 19:33:37 -0300

    Adicionados exemplos dos tipos primitivos e casting

--- tipos-variaveis/src/estudos/TiposVariaveis.java
+++ tipos-variaveis/src/estudos/TiposVariaveis.java
@@ -3,8 +3,17 @@ package estudos;
 public class TiposVariaveis {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		byte idade = 123;
+		short ano = 2021;
+		int cep = 21070333; // se começar com zero, talvez tenha que ser outro tipo
+		long cpf = 98765432109L; // se começar com zero, talvez tenha que ser outro tipo
+		float pi = 3.14F;
+		double salario = 1275.33;
+		
+		short numeroCurto = 1;
+		int numeroNormal = numeroCurto;
+		short numeroCurto2 = (short) numeroNormal // casting;
 	}
 
 }
