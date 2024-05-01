497d09d2ddbc4f08ba0ce5d0b2f2d7c4f7906b56
ERICOPAGANINI <erico.paganini@gmail.com>
2024-05-01 12:29:17 -0300

    Adicionado o valor da constante PI

--- tipos-variaveis/src/estudos/TiposVariaveis.java
+++ tipos-variaveis/src/estudos/TiposVariaveis.java
@@ -3,7 +3,7 @@ package estudos;
 public class TiposVariaveis {
 
 	public static void main(String[] args) {
-
+		// tipos primitivos e variaveis
 		byte idade = 123;
 		short ano = 2021;
 		int cep = 21070333; // se começar com zero, talvez tenha que ser outro tipo
@@ -13,7 +13,13 @@ public class TiposVariaveis {
 		
 		short numeroCurto = 1;
 		int numeroNormal = numeroCurto;
-		short numeroCurto2 = (short) numeroNormal // casting;
+		short numeroCurto2 = (short) numeroNormal;// casting;
+		
+		//constantes
+		
+		final double VALOR_DE_PI = 3.14; // a palavra reservada final faz o valor ficar constante
+		
+		
 	}
 
 }
