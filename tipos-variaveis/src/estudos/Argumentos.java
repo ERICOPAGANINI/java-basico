281f93bb4d9776b0137669629ff54e66bf2d387c
ERICOPAGANINI <erico.paganini@gmail.com>
2024-05-03 18:55:19 -0300

    Modificacao no algoritimo Argumentos

--- tipos-variaveis/src/estudos/Argumentos.java
+++ tipos-variaveis/src/estudos/Argumentos.java
@@ -5,8 +5,8 @@ public class Argumentos {
 	public static void main (String[] args) {
 		String nome =  args [0];
 		String sobrenome = args[1];
-		String idade = args[2];
-		String altura = args[3];
+		Integer idade = Integer.valueOf(args[2]);
+		Double altura = Double.valueOf(args[3]);
 		
 		System.out.println("Ola, me chamo " + nome + " " + sobrenome);
 		System.out.println("Tenho " + idade + " de idade e " + altura + "m de altura.");
