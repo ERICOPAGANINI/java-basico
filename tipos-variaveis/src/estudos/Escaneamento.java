a4ef417921f0c141c79a2d3599a9f12b166ec90b
ERICOPAGANINI <erico.paganini@gmail.com>
2024-05-03 19:07:07 -0300

    Estudo metodo Scanner

---
+++ tipos-variaveis/src/estudos/Escaneamento.java
@@ -0,0 +1,33 @@
+package estudos;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+
+public class Escaneamento {
+	public static void Mian(String[] args) {
+
+		Scanner scanner = new Scanner(System.in).useLocale(Locale.getDefault());
+
+		System.out.println("Digite seu nome");
+		String nome = scanner.next();
+
+		System.out.println("Digite seu sobrenome");
+		String sobrenome = scanner.next();
+
+		System.out.println("Digite sua idade");
+		int idade = scanner.nextInt();
+
+		System.out.println("Digite sua altura");
+		double altura = scanner.nextDouble();
+		
+		scanner.close();
+
+		// imprimindo os dados obtidos pelo usuario
+		System.out.println("Ola, me chamo " + nome + " " + sobrenome);
+		System.out.println("Tenho " + idade + " anos ");
+		System.out.println("Minha altura é " + altura + "cm ");
+
+	}
+
+}
